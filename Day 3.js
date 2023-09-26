14c8136aeb9794242f34399061d352c8a06ccb1c
Mohankrishna1012 <144467561+Mohankrishna1012@users.noreply.github.com>
2023-09-27 02:02:07 +0530

    Add files via upload

---
+++ Day 3.js
@@ -0,0 +1,12 @@
+let obj1 = { name: "person 1", age: 5 };
+let obj2 = { age: 5, name: "person 1" };
+
+var json1 = obj1
+var json2 = obj2
+
+
+if (jsonString1 === jsonString2) {
+  console.log("JSON objects are equal.");
+} else {
+  console.log("JSON objects are not equal.");
+}
