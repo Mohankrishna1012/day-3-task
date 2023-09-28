16c6e8ba0faec395d56cfd1175231a13f33bbef1
Mohankrishna1012 <144467561+Mohankrishna1012@users.noreply.github.com>
2023-09-28 19:51:05 +0530

    script1.js

--- script1.js
+++ script1.js
@@ -2,6 +2,10 @@ var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
 request.onload=function(){
-    var result = JSON.parse(request.response);
-    console.log(result);  
-}
+    var restcountries = JSON.parse(request.response); 
+    console.log(restcountries.map((country) => country.region));
+    console.log(restcountries.map((country) => country.subregion));
+    console.log(restcountries.map((country) => country.population));
+    console.log(restcountries.map((country) => country.name.common));
+ 
+};
