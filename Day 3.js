4889ccc0161821e7ed1faae6e9500af992a4c93d
Mohankrishna1012 <144467561+Mohankrishna1012@users.noreply.github.com>
2023-09-29 13:41:38 +0530

    Day 3.js

--- Day 3.js
+++ Day 3.js
@@ -1,12 +1,13 @@
 let obj1 = { name: "person 1", age: 5 };
 let obj2 = { age: 5, name: "person 1" };
 
-var json1 = obj1
-var json2 = obj2
+
+let obj1Str = JSON.stringify(obj1);
+let obj2Str = JSON.stringify(obj2);
 
 
-if (jsonString1 === jsonString2) {
-  console.log("JSON objects are equal.");
+if (obj1Str === obj2Str) {
+  console.log("The JSON objects are equal.");
 } else {
-  console.log("JSON objects are not equal.");
+  console.log("The JSON objects are not equal.");
 }
